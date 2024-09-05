84e46c07e8dcdd0ac1024bae4432ba39f543b778
Arshia <arshiamotaghian@yahoo.com>
2024-09-05 10:19:07 +0330

    New edit

--- Snappfood/food_app/serializer.py
+++ Snappfood/food_app/serializer.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Food
 
 class FoodSerializer(serializers.ModelSerializer):
-    category = serializers.StringRelatedField()
     class Meta:
         model = Food
         fields = '__all__'
--- Snappfood/food_app/views.py
+++ Snappfood/food_app/views.py
@@ -1,18 +1,21 @@
 from django.views import View
-from .models import Category,Food
+from .models import Category, Food
 from restaurant_app.models import Restaurant
 from django.http import JsonResponse
 from .serializer import FoodSerializer
-from rest_framework.generics import ListAPIView , RetrieveAPIView
+from rest_framework.generics import (
+    ListAPIView,
+    RetrieveAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListCreateAPIView,
+)
+
 
 class Foodlist(ListAPIView):
     queryset = Food.objects.all()
     serializer_class = FoodSerializer
 
 
-
-
-
-
-
-
+class Food(ListCreateAPIView):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer

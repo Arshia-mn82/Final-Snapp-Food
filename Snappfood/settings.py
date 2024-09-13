40a33e2d0f374807a757b8e524801d529f402b93
Arshia <arshiamotaghian@yahoo.com>
2024-09-13 10:29:54 +0330

    New Allowed Host

--- Snappfood/settings.py
+++ Snappfood/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG_OPTION")
 
-ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
